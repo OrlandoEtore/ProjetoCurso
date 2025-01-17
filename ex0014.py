aab8408d94151aef0741090fed9b8afb7f445a2e
OrlandoEtore <orlandoetore@gmail.com>
2025-01-17 13:55:48 -0300

    Att

--- ex0014.py
+++
@@ -1,4 +0,0 @@
-celsius = float(input("Informe a Temperatura em C: "))
-
-f = (celsius * 1.8) + 32
-print("Conversao Para F : {:.}".format(f))
--- ex011.py
+++
@@ -1,9 +0,0 @@
-larg = float(input('Largura :'))
-alt  = float(input('Altura :'))
-
-area = larg * alt
-
-pintada = area/2
-
-print('Quantidade necessaria {}'.format(area))
-print('Area pintada :{}'.format(pintada))
