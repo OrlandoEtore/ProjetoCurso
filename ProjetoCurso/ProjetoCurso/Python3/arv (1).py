5f4755533a264c7cd64fa48e83433b983abd9637
OrlandoEtore <orlandoetore@gmail.com>
2025-04-25 19:49:54 -0300

    Att

---
+++ ProjetoCurso/ProjetoCurso/Python3/arv (1).py
@@ -0,0 +1,23 @@
+nome = 'Orlando Jr'
+altura = 1.80
+peso = 91
+imc = peso / altura ** 2
+
+
+# f-strings
+linha1 = f'{nome} tem {altura:.2f} de altura'
+linha2 = f'{peso}  peso quilos e seu imc e'
+linha3 = f'{imc:.2f}'
+print(linha1)
+print(linha2)
+print(linha3)
+
+
+
+
+
+
+
+# print(nome,"tem",altura,'de altura,',)
+# print('pesa', peso, 'quilos e seu imc e',)
+# print(imc)
